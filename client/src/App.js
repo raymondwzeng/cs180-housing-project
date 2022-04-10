bc7c6e8677633fa91b7df262a8bc53916991166a
chatmansave@gmail.com <chatmansave@gmail.com>
2022-04-09 21:49:05 -0700

    Added some comments to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 function App() {
+  // Barebones HTML for the webpage
   return (
     <div className="main-container">
     <h1>Client Webpage</h1>
@@ -15,6 +16,8 @@ function App() {
   );
 }
 
+// This is a placeholder function to test the button's functionality
+// Replace with a function that handles a POST request from the client
 function changeOutputText(){
   var inputText = document.getElementById("input").value;
   document.getElementById("output").innerText = inputText;
