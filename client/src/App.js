03e8ba7fe3e1a4c3868b780c0efb432e4109093a
chatmansave@gmail.com <chatmansave@gmail.com>
2022-04-18 01:14:34 -0700

    Can now get min and max values from 2-sided sliders

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,84 @@
 import './App.css';
+import './Slider.css'
+import ReactSlider from 'react-slider'
 
 function App() {
   // Barebones HTML for the webpage
   return (
-    <div className="main-container">
-    <h1>Client Webpage</h1>
-    <form>
-      <label>
-        <input type="text" id="input" placeholder="Input" />
-      </label>
-    </form>
-    <input type="submit" value="Submit input to server" onClick={changeOutputText}/>
-    <p id="output"> Result from server</p>
+    <div className="page-contents">
+      <h1>1990 Housing Data Viewer</h1>
+      <div className='test'>10</div>
+      {/* Two Sided Sliders for all of the data values*/}
+      <div className="slider">
+        <div className="slider-text">Median House Price</div>
+        <TwoSidedSlider className="median-slider" min={0} max={100}/>
+      </div>
+      <div className="slider">
+        <div className="slider-text">Latitude</div>
+        <TwoSidedSlider className="latitude-slider" min={0} max={100}/>
+      </div>
+      <div className="slider">
+        <div className="slider-text">Longitude</div>
+        <TwoSidedSlider className="longitude-slider" min={0} max={100}/>
+      </div>
+      <input className="button" type="submit" value="Submit input to server" onClick={changeOutputText}/>
+      <p id="output"> Result from server</p>
     </div>
+  );
+}
+
+// Returns a Slider with two thumbs
+function TwoSidedSlider(props) {
+  return (
+    <ReactSlider
+        className={"horizontal-slider " + props.className}
+        id={props.id}
+        thumbClassName={"slider-thumb " + props.className + "-value"}
+        trackClassName="slider-track"
+        defaultValue={[props.min, props.max]}
+        min={props.min}
+        max={props.max}
+        ariaLabel={['Lower thumb', 'Upper thumb']}    // aria text for screen readers
+        ariaValuetext={state => `Thumb value ${state.valueNow}`}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        pearling
+        // onAfterChange={(value, index) =>
+        //   console.log(`onAfterChange: ${JSON.stringify({ value, index })}`)
+        // }
+        // minDistance={1}
+      />
   );
 }
 
 // This is a placeholder function to test the button's functionality
 // Replace with a function that handles a POST request from the client
-async function changeOutputText(){
-  var inputText = document.getElementById("input").value;
-  let response = await postFunc('api/test', inputText);
-  document.getElementById("output").innerText = response;
-}
-async function postFunc(req, sendText){
-  try {
-    let response = await fetch('http://localhost:4000/'+req,{
-      method: 'Post',
-      headers:{
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        firstParam:sendText
-      })
-    })
+function changeOutputText(){
 
-  //turn the POST response into a json file, and return the firstParam
-  
-    let data = await response.json()
-    console.log(data.firstParam);
-    return data.firstParam;
-  }
-  catch(error) {
-    console.error("ERROR: No response from server. Please check if server is running.");
-    return "ERROR: No Response From Server";
-  }
-  
+  // Minimum and maximum values from all of the sliders
+  // We might find a better way of passing these values to the server
+  var medianMin = document.getElementsByClassName("median-slider-value-0")[0].innerText;
+  var medianMax = document.getElementsByClassName("median-slider-value-1")[0].innerText;
+  var longitudeMin = document.getElementsByClassName("longitude-slider-value-0")[0].innerText;
+  var longitudeMax = document.getElementsByClassName("longitude-slider-value-1")[0].innerText;
+  var latitudeMin = document.getElementsByClassName("latitude-slider-value-0")[0].innerText;
+  var latitudeMax = document.getElementsByClassName("latitude-slider-value-1")[0].innerText;
+
+  postFunc('api/test', medianMin);
+  document.getElementById("output").innerText = medianMin;
+}
+
+function postFunc(req, sendText){
+  fetch('http://localhost:4000/'+req,{
+    method: 'Post',
+    headers:{
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      firstParam:sendText
+    })
+  });
+  //return to be implement for handling info
 }
 
 export default App;
