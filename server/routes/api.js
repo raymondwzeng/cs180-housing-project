011690ab7e97d7c4e5808e9894e16ca2b52fd1aa
roveredpwn <roverdog56@gmail.com>
2022-04-25 01:35:49 -0700

    ACTUALLY Changed update api call to be more or less correct

--- server/routes/api.js
+++ server/routes/api.js
@@ -26,7 +26,7 @@ router.post('/getFilteredData', (req, res) => {
 
 /*
 API call for the create function in the operations layer.
-req: An data array with 15 numbers that can be passed into the neighborhood constructor.
+req: An array with 15 numbers that can be passed into the neighborhood constructor.
 res: string message saying whether or not create operation was successful.
 */
 router.post('/cards', (req, res) => {
@@ -56,20 +56,18 @@ router.delete('/cards', (req, res) => {
 
 /*
 API call for the update function in the operations layer.
-req: 
-    1) The id of the neighborhood to update within the neighborhoodList.
-    2) The column number to update (a number from 0 to 13)
-    3) The value to replace it with
+req: An array of the 15 values needed to for the neighborhood constructor.
+    id value within the array will be the one that will be updated in the NeighborhoodList.
 res: string message saying whether or not delete operation was successful.
 */
 router.patch('/cards', (req, res) => {
     console.log(req.body)
     // result var store operation status. 0 means success, -1 means failure
-    var result = OperationsLayer.update(req.body[0], req.body[1], req.body[2])
+    var result = OperationsLayer.update(req.body)
     if (result == 0)
-        res.json("Successfully updated neighborhood with id: " + req.body[0] + ", at column: " + req.body[1] + ", with value: " + req.body[2])
+        res.json("Successfully updated neighborhood with id: " + req.body.id)
     else
-        res.json("ERROR: unable to update neighborhood with id of " + req.body[0])
+        res.json("ERROR: unable to update neighborhood with id: " + req.body.id)
 })
 
 module.exports = router
