6459bb6a371fe803741fe712b3682f84b248e939
tgustafson2 <gus.duckfan@gmail.com>
2022-04-30 17:33:09 -0700

    Changed for loops to forEach functions

--- server/analytics.js
+++ server/analytics.js
@@ -9,79 +9,49 @@ function getColumn(constraintArray,x){
 	let column=[]
 	switch(x){
 		case 1:
-			for(let i=0;i<list.length;i++){
-				column.push(parseInt(list[i].median_value));
-			}
+				list.forEach(element => column.push(parseInt(element.median_value)));
 			break;
 		case 2:
-			for(let i=0;i<list.length;i++){
-				column.push(parseFloat(list[i].median_income));
-			}
+				list.forEach(element => column.push(parseFloat(element.median_income)));
 			break;
 		case 3:
-			for(let i=0;i<list.length;i++){
-				column.push(parseInt(list[i].median_age));
-			}
+				list.forEach(element => column.push(parseInt(element.median_age)));
 			break;
 		case 4:
-			for(let i=0;i<list.length;i++){
-				column.push(parseInt(list[i].total_rooms));
-			}
+				list.forEach(element => column.push(parseInt(element.total_rooms)));
 			break;
 		case 5:
-			for(let i=0;i<list.length;i++){
-				column.push(parseInt(list[i].total_bedrooms));
-			}
+				list.forEach(element => column.push(parseInt(element.total_bedrooms)));
 			break;
 		case 6:
-			for(let i=0;i<list.length;i++){
-				column.push(parseInt(list[i].population));
-			}
+				list.forEach(element => column.push(parseInt(element.population)));
 			break;
 		case 7:
-			for(let i=0;i<list.length;i++){
-				column.push(parseInt(list[i].households));
-			}
+				list.forEach(element => column.push(parseInt(element.households)));
 			break;
 		case 8:
-			for(let i=0;i<list.length;i++){
-				column.push(parseFloat(list[i].latitude));
-			}
+				list.forEach(element => column.push(parseFloat(element.latitude)));
 			break;
 		case 9:
-			for(let i=0;i<list.length;i++){
-				column.push(parseFloat(list[i].longitude));
-			}
+				list.forEach(element => column.push(parseFloat(element.longitude)));
 			break;
 		case 10:
-			for(let i=0;i<list.length;i++){
-				column.push(parseFloat(list[i].distance_to_coast));
-			}
+				list.forEach(element => column.push(parseFloat(element.distance_to_coast)));
 			break;
 		case 11:
-			for(let i=0;i<list.length;i++){
-				column.push(parseFloat(list[i].distance_to_LA));
-			}
+				list.forEach(element => column.push(parseFloat(element.distance_to_LA)));
 			break;
 		case 12:
-			for(let i=0;i<list.length;i++){
-				column.push(parseFloat(list[i].distance_to_SD));
-			}
+				list.forEach(element => column.push(parseFloat(element.distance_to_SD)));
 			break;
 		case 13:
-			for(let i=0;i<list.length;i++){
-				column.push(parseFloat(list[i].distance_to_SJ));
-			}
+				list.forEach(element => column.push(parseFloat(element.distance_to_SJ)));
 			break;
 		case 14:
-			for(let i=0;i<list.length;i++){
-				column.push(parseFloat(list[i].distance_to_SF));
-			}
+				list.forEach(element => column.push(parseFloat(element.distance_to_SF)));
 			break;
 		case 15:
-			for(let i=0;i<list.length;i++){
-				column.push(parseInt(list[i].id));
-			}
+				list.forEach(element => column.push(parseInt(element.id)));
 			break;
 	}
 	return column;
