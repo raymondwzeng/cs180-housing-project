4849d2812de6900778852b557a0906eae9d6a53e
MacAndSwiss <rzeng3@gmail.com>
2022-05-01 20:20:02 -0700

    Major changes to presentation, use GET request to server for data

--- client/src/routes/Graphs.js
+++ client/src/routes/Graphs.js
@@ -4,29 +4,30 @@
     The place where we will be comparing and contrasting various columns
 */
 
-import { GlyphSeries, XYChart, Tooltip } from "@visx/xychart"
+import { GlyphSeries, XYChart, Tooltip, Axis } from "@visx/xychart"
 import { Component } from "react";
 import Dropdown from "../components/Dropdown"
+import { defaultHeaders } from "./Data";
 
-const items = ["1", "2", "3"]
+const items = [
+    "Median_House_Value", 
+    "Median_Income", 
+    "Median_Age", 
+    "Tot_Rooms",
+    "Tot_Bedrooms",
+    "Population",
+    "Households",
+    "Latitude",
+    "Longitude",
+    "Distance_to_coast",
+    "Distance_to_LA",
+    "Distance_to_SanDiego",
+    "Distance_to_SanJose",
+    "Distance_to_SanFrancisco"]
 
-const data1 = [
-    { x: '2020-01-01', y: 50 },
-    { x: '2020-01-02', y: 10 },
-    { x: '2020-01-03', y: 20 },
-  ];
-  
-  const data2 = [
-    { x: '2020-01-04', y: 30 },
-    { x: '2020-01-06', y: 40 },
-    { x: '2020-01-05', y: 80 },
-  ];
-
-  const data3 = [
-    { x: '2020-01-02', y: 30 },
-    { x: '2020-01-03', y: 60 },
-    { x: '2020-01-08', y: 80 },
-  ];
+const units = {
+    "Median_Income": "(thousands of USD)"
+}
   
   const accessors = {
     xAccessor: d => d.x,
@@ -37,61 +38,88 @@ class Graph extends Component{
     constructor(props) {
         super(props)
         this.state = {
-            selectedData: data1
+            selectedColumn: "",
+            currentData: []
         }
-
         this.changeSelectedData = this.changeSelectedData.bind(this)
+        this.getColumnFromServer = this.getColumnFromServer.bind(this)
+    }
+
+    /**
+     * getMedianValueFromServer
+     * 
+     * Shouldn't be executed so many times, but we can figure that out in a future sprint™
+     * 
+     * @param {Array} newData - The empty data table to fill up. 
+     */
+    async getMedianValueFromServer(newData) {
+        const params = new URLSearchParams({column_name: "Median_House_Value"})
+        await fetch(`http://localhost:4000/api/column?${params}`, {
+            method: "GET",
+            headers: defaultHeaders
+        }).then(resp => resp.json())
+        .then(responseJSON => {
+            responseJSON.forEach(element => {
+                newData.push({y: element})
+            });
+        })
+    }
+
+    /**
+     * getColumnFromServer
+     * 
+     * No parameters.
+     * TODO: Avoid calling the API twice to get the housing data for median home price (since that's fixed)
+     */
+    async getColumnFromServer(){
+        let newData = []
+        this.setState({currentData: []}) //Erase data
+        await this.getMedianValueFromServer(newData)
+        const params = new URLSearchParams({column_name: this.state.selectedColumn})
+        await fetch(`http://localhost:4000/api/column?${params}`, {
+            method: "GET",
+            headers: defaultHeaders
+        }).then(resp => resp.json())
+        .then(responseJSON => {
+            let index = 0;
+            responseJSON.forEach(element => {
+                newData[index++].x = element
+            })
+            this.setState({currentData: newData.slice(0, 2000).sort((a, b) => a.x - b.x)})
+        })
     }
 
     changeSelectedData(event) {
-        let newSelectedData
-        console.log(event.target.innerText)
-        switch(event.target.innerText) {
-            case "1":
-                newSelectedData = data1
-                break;
-            case "2":
-                newSelectedData = data2
-                break;
-            case "3":
-                newSelectedData = data3
-                break;
-            default:
-                newSelectedData = data1
-        }
+        const innerText = event.target.innerText.replaceAll(" ", "_")
         this.setState({
-            selectedData: newSelectedData
+            selectedColumn: innerText
         })
+        this.getColumnFromServer()
     }
 
 
     render() {
         return(
         <div>
-            <h3>Choose a column to compare against median housing:</h3>
-            <Dropdown items={items} changed={this.changeSelectedData}/>
+            <div className="flex-horizontal">
+            <div className="big-font small-horizontal-padding">Choose a column to compare against median housing:</div>
+            <Dropdown items={items} changed={this.changeSelectedData} selected={this.state.selectedColumn}/>
+            </div>
 
             <div id="graphs">
-                {/* <XYChartTest/> */}
                 <XYChart height={300} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
-                    <GlyphSeries data={this.state.selectedData} xAccessor={accessors.xAccessor} yAccessor={accessors.yAccessor}/>
-                    {/* <AnimatedAxis orientation="bottom" /> */}
+                    <GlyphSeries data={this.state.currentData} {...accessors}/>
+                    <Axis key={`axis-bottom`} label={this.state.selectedColumn.replaceAll("_", " ") + (units[this.state.selectedColumn] != null ? units[this.state.selectedColumn] : "")} orientation="bottom"/>
+                    <Axis key={`axis-side`} label="Median House Value" orientation="left"/>
                     <Tooltip
-                        snapTooltipToDatumX
-                        snapTooltipToDatumY
-                        showVerticalCrosshair
                         showSeriesGlyphs
-                        renderTooltip={({ tooltipData, colorScale }) => (
+                        renderTooltip={({ tooltipData, _ }) => (
                             <div>
-                            <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
-                                {tooltipData.nearestDatum.key}
-                            </div>
                             {accessors.xAccessor(tooltipData.nearestDatum.datum)}
                             {', '}
                             {accessors.yAccessor(tooltipData.nearestDatum.datum)}
                             </div>
                         )}/>
-                    {/* TODO: Fill out */}
                 </XYChart>
             </div>
         </div>
