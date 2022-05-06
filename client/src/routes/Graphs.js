236b645e03463a689d67a83cdce333ef314b41f5
Raymond Zeng <rzeng3@gmail.com>
2022-05-06 11:46:30 -0700

    Cosmetic changes to graphs page

--- client/src/routes/Graphs.js
+++ client/src/routes/Graphs.js
@@ -145,6 +145,7 @@ class Graph extends Component{
     render() {
         return(
         <div>
+            <h1>1990 Housing Data Viewer - Graphs</h1>
             <div className="flex-horizontal">
             <div className="big-font small-horizontal-padding">Choose a column to compare against median housing:</div>
             <Dropdown items={items} changed={this.changeSelectedData} selected={this.state.selectedColumn}/>
@@ -173,7 +174,6 @@ class Graph extends Component{
                         showSeriesGlyphs
                         renderTooltip={({tooltipData, _}) => (
                                 <div>
-                                    {console.log(tooltipData.nearestDatum)}
                                     {'Value: '}
                                     {accessors.xAccessor(tooltipData.nearestDatum.datum)}
                                     {'\n Occurrences: '}
