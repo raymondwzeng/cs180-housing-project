89c597e9dcb45191c93c4694fe06a170ce2c7d9c
dukeph0 <dpham5161@outlook.com>
2022-05-09 02:30:02 -0700

    Remove logo button

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,20 +4,9 @@ import React from "react";
 const Navbar = () => {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="">
+      <a className="navbar-brand">
         1990 Housing Data
       </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
 
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
