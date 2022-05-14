1df774c8cadb380490d1efb1dd417c02d5ca5f39
MacAndSwiss <rzeng3@gmail.com>
2022-05-14 14:42:00 -0700

    CSS mini-overhaul, not complete

--- client/src/routes/Graphs.js
+++ client/src/routes/Graphs.js
@@ -149,8 +149,7 @@ class Graph extends Component{
             <div id='navigation-bar'>
                 <Navbar />
             </div>
-            <h1>1990 Housing Data Viewer - Graphs</h1>
-            <div className="flex-horizontal">
+            <div className="flex-horizontal small-margin-top">
             <div className="big-font small-horizontal-padding">Choose a column to compare against median housing:</div>
             <Dropdown items={items} changed={this.changeSelectedData} selected={this.state.selectedColumn}/>
             </div>
